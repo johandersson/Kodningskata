73d5095c401b02276891bc5592ac74f8760fa214
Johan Andersson <johanbkandersson@gmail.com>
2023-03-02 14:38:18 +0100

    Added text file with piratedecrypted text

--- PirateLang/PirateLang/PirateDecrypter.cs
+++ PirateLang/PirateLang/PirateDecrypter.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 namespace PirateLang
 {
-    class PirateDecrypter
+    public class PirateDecrypter
     {
         public PirateDecrypter()
         {
---
+++ PirateLang/PirateLangConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using PirateLang;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PirateLangConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pirateDecrypter = new PirateDecrypter();
+            var textFileWithPirateLanguageReader = new StreamReader("pirate-encrypted.txt");
+            string pirateString = textFileWithPirateLanguageReader.ReadToEnd();
+            Console.WriteLine(pirateDecrypter.DecryptPirateString(pirateString));
+        }
+    }
+}
